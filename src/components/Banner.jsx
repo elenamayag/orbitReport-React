971e1d620ce1057a511e98ced4f6395f557bda4c
elenamayag <142683768+elenamayag@users.noreply.github.com>
2023-11-18 21:32:02 -0600

    orbit report, styling done. hopefully runs smoothly...

---
+++ src/components/Banner.jsx
@@ -0,0 +1,20 @@
+
+
+import React from "react";
+import './styling.css';
+
+
+
+
+function Banner () {
+    return (
+    <header className="banner">
+        <h1>Orbit Report</h1>
+        <p>
+            Click on the buttons to see the satellites in that orbit type.
+        </p>
+    </header>
+    );
+};
+
+export default Banner;
